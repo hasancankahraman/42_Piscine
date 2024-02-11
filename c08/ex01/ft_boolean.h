bfa05020451754273dc407ec775199cddf238cab
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-11 16:58:35 +0300

    c08

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 11:22:18 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/02/07 11:26:14 by hkahrama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN(nbr) (nbr % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+typedef int	t_bool;
+
+#endif /* FT_BOOLEAN_H */
---
+++ c08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 11:22:18 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/02/07 11:26:14 by hkahrama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+		++i;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dest)
+		return (NULL);
+	i = -1;
+	while (src[++i])
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*struct_arr;
+	int			i;
+
+	struct_arr = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!struct_arr)
+		return (NULL);
+	i = -1;
+	while (++i < ac)
+	{
+		struct_arr[i].size = ft_strlen(av[i]);
+		struct_arr[i].str = av[i];
+		struct_arr[i].copy = ft_strdup(av[i]);
+	}
+	struct_arr[i].size = 0;
+	struct_arr[i].str = 0;
+	struct_arr[i].copy = 0;
+	return (struct_arr);
+}
+
+/*
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	t_stock_str *struct_arr = ft_strs_to_tab(argc, argv);
+
+	int i = -1;
+	while (struct_arr[++i].str)
+	{
+		printf("%s\n", struct_arr[i].str);
+	}
+}
+*/
