19e90114681dfa58181fbcdf94480cba4b83f9a3
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-11 21:06:44 +0300

    ft_print_comb2.c Updated

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -5,38 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/25 20:02:12 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/01/25 20:44:32 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/25 20:30:12 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/25 20:38:32 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char a)
+void	ft_putchar(char a) // Fonksiyonumuzda bir char parametresi istiyoruz
 {
-	write(1, &a, 1);
+	write(1, &a, 1); // Ekrana a değişkenini yazdır
 }
 
 void	ft_print_comb2(void)
 {
-	int		a;
-	int		b;
+	int	a; // a diye bir int değişkeni tanımlandı
+	int	b; // a diye bir int değişkeni tanımlandı
 
-	a = -1;
-	while (a++ < 98)
+	a = -1; // a değişkeninin değeri -1 oldu çünkü istenen alıştırmada a'nın 0'dan başlaması gerekiyor
+	while (a++ < 98) // a değişkeninin 1 fazlası 98'den küçükse döngüye gir çünkü istenen alıştırmada a'nın alabileceği maksimum değer 98
 	{
-		b = a;
-		while (b++ < 99)
+		b = a; // b değişkeni a değişkenin değerini aldı
+		while (b++ < 99) // b değişkeninin 1 fazlası 99'dan küçükse döngüye gir çünkü istenen alıştırmada b'nin alabileceği maksimum değer 99
 		{
-			ft_putchar((a / 10) + 48);
-			ft_putchar((a % 10) + 48);
-			ft_putchar(' ');
-			ft_putchar((b / 10) + 48);
-			ft_putchar((b % 10) + 48);
-			if (a != 98)
+			ft_putchar((a / 10) + 48); // a değişkenimizi 10'a bölüp gelen int değerini char değişkenine çevirebilmek için 0 sayısının ascii değeri olan 48 ile toplayarak ascii'ye uygun olacak şekilde yazdırıyoruz
+			ft_putchar((a % 10) + 48); // a değişkenimizin 10'a bölümünden kalan int değerini char değişkenine çevirebilmek için 0 sayısının ascii değeri olan 48 ile toplayarak ascii'ye uygun olacak şekilde yazdırıyoruz
+			ft_putchar(' '); // boşluk yazdırıyoruz
+			ft_putchar((b / 10) + 48); // b değişkenimizi 10'a bölüp gelen int değerini char değişkenine çevirebilmek için 0 sayısının ascii değeri olan 48 ile toplayarak ascii'ye uygun olacak şekilde yazdırıyoruz
+			ft_putchar((b % 10) + 48); // b değişkenimizin 10'a bölümünden kalan int değerini char değişkenine çevirebilmek için 0 sayısının ascii değeri olan 48 ile toplayarak ascii'ye uygun olacak şekilde yazdırıyoruz
+			if (a != 98) // eğer a değişkeni 98'e eşit değilse
 			{
-				ft_putchar(',');
-				ft_putchar(' ');
+				ft_putchar(','); // virgül yazdırıyoruz
+				ft_putchar(' '); // boşluk yazdırıyoruz
 			}
 		}
 	}
