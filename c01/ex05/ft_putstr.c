7e8cbf0fd96789c9b4f92c353abfd5bc753a0797
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-12 20:46:43 +0300

    ft_putstr.c Updated

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -5,23 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:10:00 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:25:04 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/26 11:10:00 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/26 11:25:04 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+void	ft_putstr(char *str) // Fonksiyonumuzda 1 adet char tipinde bir str parametresi gönderilmesini istiyoruz. Char tipindeki bu parametre pointer'lı olduğu için string halini alabiliyor.
 {
-	int	i;
+	int	n; // n adında integer tipinde bir değişken oluşturuyoruz.
 
-	i = 0;
-	while (*str)
+	n = 0; // n'in değerini 0'a eşitliyoruz.
+	while (str[n] != '\0') // Eğer str değişkeninin n değerindeki indeksini boşluğa eşit değilse bir döngüye giriyoruz.
 	{
-		write(1, str++, 1);
-		i++;
-	}
+		write (1, &str[n], 1); // str değişkeninin n değerindeki indeksini ekrana yazdırıyoruz.
+		n++; // n değişkeninin değerini 1 arttırıyoruz.
+	}	
 }
 
 /*#include <stdio.h>
