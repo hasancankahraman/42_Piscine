46016c591668873fe9f2a1481d84326292423756
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-12 21:10:45 +0300

    ft_str_is_lowercase.c  Updated

--- c02/ex04/ft_str_is_lowercase.c
+++ c02/ex04/ft_str_is_lowercase.c
@@ -5,27 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:58:25 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:58:26 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/26 13:58:25 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/26 14:10:26 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_str_is_lowercase(char *str)
+int	ft_str_is_lowercase(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer bir değişken oluşturuyoruz.
 
-	i = 0;
-	while (str[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa döngüye giriyoruz.
 	{
-		if (!(str[i] >= 'a' && str[i] <= 'z'))
+		if (!(str[i] >= 'a' && str[i] <= 'z')) // str değişkeninin i değerindeki indeksi küçük a harfinden büyük/eşit değilse ve küçük z harfinden küçük/eşit değilse bu koşula giriyoruz.
 		{
-			return (0);
+			return (0); // 0 döndürüp ekrana yazdırıyoruz.
 		}
-		i++;
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (1);
+	return (1); // 1 döndürüp ekrana yazdırıyoruz.
 }
 /*
 int	main()
