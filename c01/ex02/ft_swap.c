58e5b2b8b4e90963d7a0177532eb9cd2f30ac985
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-12 20:43:59 +0300

    ft_swap.c Updated

--- c01/ex02/ft_swap.c
+++ c01/ex02/ft_swap.c
@@ -5,21 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 10:24:40 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 10:33:44 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/26 10:24:40 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/26 10:33:44 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_swap(int *a, int *b)
+void	ft_swap(int *a, int *b) // Fonksiyonumuzda 2 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	int	temp;
+	int	tmp; // tmp adında integer tipinde bir değişken oluşturuyoruz.
 
-	temp = *a ;
-	*a = *b;
-	*b 
-	= temp;
+	tmp = *a; // Fonksiyonda gönderilen a parametresinin değerini tmp değişkenine eşitliyoruz.
+	*a = *b; // Fonksiyonda gönderilen b parametresinin değerini a değişkenine eşitliyoruz.
+	*b = tmp; // İlk başta a parametresinden gelen değeri tmp'ye göndermiştik şimdi de tmp değerini b değişkenine eşitliyoruz.
+	// Ve bu sayede fonksiyonda gönderilen a parametresindeki değer ile b parametresindeki değer yer değiştirmiş oluyor.
 }
 /*
 #include <stdio.h>
