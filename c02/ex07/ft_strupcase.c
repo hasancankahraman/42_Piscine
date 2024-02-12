5eb8c2e2943f6500a9059f0c180d70742f38c361
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-12 21:17:25 +0300

    ft_strupcase.c Updated

--- c02/ex07/ft_strupcase.c
+++ c02/ex07/ft_strupcase.c
@@ -1,35 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_trupcase.c                                      :+:      :+:    :+:   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:58:25 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:58:26 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/26 15:30:25 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/26 15:40:26 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strupcase(char *str)
+char	*ft_strupcase(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	while (str[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa döngüye giriyoruz.
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z') // str değişkeninin i değerindeki indeksi küçük a harfinden büyükse/eşitse ve küçük z harfinden küçükse/eşitse bu koşula giriyoruz.
 		{
-			str[i] -= 32;
+			str[i] -= 32; // str değişkeninin i değerinindeki indeksinde bulunan alfabetik karakteri büyütmek için ASCII tablosunda 32 değer eksiltiyoruz.
 		}
-		i++;
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (str);
+	return (str); // str değişkeninin değerini döndürüp ekrana yazdırıyoruz.
 }
-
-int	main()
+/*int	main()
 {
-	char str[] = "akifdora";
+	char str[] = "hasank";
 	printf("%s", ft_strupcase(str));
-}
+}*/
