f7f39ae1479be54f51fd73815c9c662b9458e446
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-12 20:44:51 +0300

    ft_div_mod.c Updated

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -5,19 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 10:38:48 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 10:59:06 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/26 10:38:48 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/26 10:59:06 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_div_mod(int a, int b, int *div, int *mod)
+void	ft_div_mod(int a, int b, int *div, int *mod) // Fonksiyonumuzda 4 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	if (b != 0)
+	if (b != 0) // Eğer b parametresinin değeri 0'a eşit değilse
 	{
-		*div = a / b;
-		*mod = a % b;
+		*div = a / b; // a parametresinin değerinin b parametresinin değerine bölümünden çıkan değeri div değişkenine atıyoruz.
+		*mod = a % b; // a parametresinin değerinin b parametresinin değerine modundan çıkan değeri mod değişkenine atıyoruz.
 	}
 }
 
