d6eacf10ad3c7422a21b3d683e17d2698d2762a9
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-12 21:04:17 +0300

    ft_strcpy.c Updated

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -5,25 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:22:18 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:26:14 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/26 13:22:18 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/26 13:28:14 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-char	*ft_strcpy(char *dest, char *src)
+char	*ft_strcpy(char *dest, char *src) // Fonksiyonumuzda 2 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer tipinde bir değişken oluşturuyoruz.
 
-	i = 0;
-	while (src[i])
+	i = 0; // i'nin değerini 0'a eşitliyoruz.
+	while (src[i]) // Eğer src değişkeninin i değerindeki indeksi varsa bir döngüye giriyoruz.
 	{
-		dest[i] = src[i];
-		i++;
+		dest[i] = src[i]; // src değişkeninin i değerindeki indeksini dest değişkeninin i değerindeki indeksine gönderiyoruz.
+		i++; // i değişkenini 1 arttırıyoruz.
 	}
-	dest[i] = '\0';
-	return (dest);
+	dest[i] = '\0'; // Kopyalama işlemi bitip, döngüden çıktıktan sonra dest değişkeninin i değerindeki indeksini boşluğa eşitliyoruz.
+	return (dest); // dest değerini döndürüp ekrana yazdırıyoruz.
 }
 
 /*#include <stdio.h>
