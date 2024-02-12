ce5ed9537fba1c962b32d2dc7ae917f9481d0d5b
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-12 20:45:53 +0300

    ft_ultimate_div_mod.c Updated

--- c01/ex04/ft_ultimate_div_mod.c
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -5,22 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:00:53 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:09:19 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/26 11:00:53 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/26 11:09:19 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_ultimate_div_mod(int *a, int *b)
+void	ft_ultimate_div_mod(int *a, int *b) // Fonksiyonumuzda 2 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	int	div;
-	int	mod;
+	int	div; // div adında integer tipinde bir değişken oluşturuyoruz.
+	int	mod; // mod adında integer tipinde bir değişken oluşturuyoruz.
 
-	div = *a / *b;
-	mod = *a % *b;
-	*a = div;
-	*b = mod;
+	div = *a / *b; // a parametresinin değerinin b parametresinin değerine bölümünden çıkan değeri div değişkenine atıyoruz.
+	mod = *a % *b; // a parametresinin değerinin b parametresinin değerine modundan çıkan değeri mod değişkenine atıyoruz.
+	*a = div; // div değişkenindeki değeri a değişkenine atıyoruz.
+	*b = mod; // mod değişkenindeki değeri b değişkenine atıyoruz.
 }
 
 /*#include <stdio.h>
