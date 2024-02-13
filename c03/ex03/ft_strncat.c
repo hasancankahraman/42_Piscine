825ea5bc18917c64c30d33beaa36e6f04bcf6663
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-13 19:46:10 +0300

    ft_strncat.c Updated

--- c03/ex03/ft_strncat.c
+++ c03/ex03/ft_strncat.c
@@ -5,34 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:22:18 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:26:14 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/27 12:22:18 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/27 12:26:14 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char    *ft_strncat(char *dest, char *src, unsigned int nb)
+char	*ft_strncat(char *dest, char *src, unsigned int nb) // Fonksiyonumuzda 2 adet char ve 1 adet unsigned integer tipinde parametre gönderilmesini istiyoruz.
 {
-    unsigned int    x;
-    unsigned int    y;
+	unsigned int	x; // x adında unsigned integer tipinde bir değişken tanımlıyoruz.
+	unsigned int	y; // y adında unsigned integer tipinde bir değişken tanımlıyoruz.
 
-    x = 0;
-    y = 0;
-    while (dest[x])
-    x++;
-    while (y < nb && src[y])
-    {
-        dest[x + y] = src[y];
-        y++;
-    }
-    dest[x + y] = '\0';
-    return (dest);
+	x = 0; // x değişkenini 0'a eşitliyoruz.
+	y = 0; // y değişkenini 0'a eşitliyoruz.
+	while (dest[x]) // dest değişkeninin x indeksindeki değeri varsa bu koşula giriyoruz.
+	x++; // x değerini 1 arttırıyoruz.
+	while (y < nb && src[y]) // y değişkeninin değeri nb değişkeninin değerinden küçükse ve src değişkeninin y değerindeki indeksi varsa bu koşula giriyoruz.
+	{
+		dest[x + y] = src[y]; // src değişkenin y değerindeki indeksini dest değişkeninin x ve y değerinin toplamındaki indeksine eşitliyoruz.
+		y++; // y değerini 1 arttırıyoruz.
+	}
+	dest[x + y] = '\0'; // dest değişkeninin x ve y değerinin toplamındaki indeksini boşluğa eşitliyoruz.
+	return (dest); // dest değişkeninin değerini döndürüp ekrana yazdırıyoruz.
 }
 
-int	main()
+/*int	main()
 {
 	char src[] = "Network";
 	char dest[] = "42 ";
 	printf("%s", ft_strncat(dest, src, 3));
-}
+}*/
