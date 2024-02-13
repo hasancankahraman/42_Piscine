fbd1bbfd4aae00ea11cf38d7b0b33c0e0d3adde3
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-13 19:41:37 +0300

    ft_strcmp.c Updated

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -5,23 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:22:18 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:26:14 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/27 11:22:18 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/27 11:26:14 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2) // Fonksiyonumuzda 2 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	unsigned int	i;
+	int	i; // i adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] && s2[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	while (s1[i] == s2[i] && s1[i] && s2[i]) // s1 değişkenin i değerindeki indeksi s2 değişkeninin i değerindeki indeksine eşitse ve s1 değişkeninin i değerindeki indeksi varsa ve s2 değişkeninin i değerindeki indeksi varsa bu koşula giriyoruz.
 	{
-		i++;
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]); // s1 değişkeninin i değerindeki indeksinden s2 değişkeninin i değerindeki indeksinin ASCII değerlerinin karşılıklarını çıkartıp döndürüyoruz ve ekrana yazdırıyoruz.
 }
 
 int	main()
