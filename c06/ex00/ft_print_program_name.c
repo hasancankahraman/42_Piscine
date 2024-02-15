c9872b576fe7a4198c32d6375f9aae4b66c7ff9c
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-15 16:25:29 +0300

    ft_print_program_name.c Updated

--- c06/ex00/ft_print_program_name.c
+++ c06/ex00/ft_print_program_name.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                                         :+:      :+:    :+:   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,21 +12,21 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char c) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	write(1, &c, 1);
+	write(1, &c, 1); // Fonksiyonun istediği char parametresini yazdırıyoruz.
 }
 
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[]) // Programımızda 1 adet integer ve 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	a;
+	int	i; // i adında integer tipinde bir değişken tanımlıyoruz.
 
-	a = 0;
-	while (argv[0][a] != '\0' && argc)
+	i = 0; // i değerini 0'a eşitliyoruz.
+	while (argv[0][i] != '\0' && argc) // Gönderilen argv dizisinin ilk stringinde i değerindeki indeksi boşluğa eşit değilse ve argc varsa bu döngüye giriyoruz.
 	{
-		ft_putchar(argv[0][a]);
-		++a;
+		ft_putchar(argv[0][i]); // ft_putchar fonksiyonunu kullanarak argv dizisinin ilk stringinde i değerindeki indeksini yazdırıyoruz.
+		++i; // i değerini 1 arttırıp devam ediyoruz.
 	}
-	ft_putchar('\n');
-	return (0);
+	ft_putchar('\n'); // Alt satıra geçiyoruz.
+	return (0); // 0 döndürüp ekrana yazdırıyoruz.
 }
