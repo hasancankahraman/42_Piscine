191ea2b8323ac9388204e3aee042711117971a92
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-15 14:25:33 +0300

    ft_strlen.c Updated

--- c04/ex01/ft_putstr.c
+++ c04/ex01/ft_putstr.c
@@ -12,16 +12,16 @@
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+void	ft_putstr(char *str) // Fonksiyonumuzda 1 adet char tipinde bir str parametresi gönderilmesini istiyoruz. Char tipindeki bu parametre pointer'lı olduğu için string halini alabiliyor.
 {
-	int	i;
+	int	i; // n adında integer tipinde bir değişken oluşturuyoruz.
 
-	i = 0;
-	while (str[i])
+	i = 0; // n'in değerini 0'a eşitliyoruz.
+	while (str[i] != '\0') // Eğer str değişkeninin n değerindeki indeksini boşluğa eşit değilse bir döngüye giriyoruz.
 	{
-		write (1, &str[i], 1);
-		i++;
-	}
+		write (1, &str[i], 1); // str değişkeninin n değerindeki indeksini ekrana yazdırıyoruz.
+		i++; // n değişkeninin değerini 1 arttırıyoruz.
+	}	
 }
 /*int	main(void)
 {
