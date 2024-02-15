a401f6152b0e953ea560acfdd0ca12d11535fe49
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-15 15:56:01 +0300

    ft_recursive_power.c Updated

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:22:18 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:26:14 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/28 10:22:43 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/28 10:33:23 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_recursive_power(int nb, int power)
+int	ft_recursive_power(int nb, int power) // Fonksiyonumuzda 2 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	int	result;
+	int	result; // result adında integer tipinde bir değişken tanımlıyoruz.
 
-	result = nb;
-	if (power > 1)
-		return (nb * ft_recursive_power(nb, (power - 1)));
-	if (power == 0)
-		return (1);
-	if (power < 0)
-		return (0);
-	return (result);
+	result = nb; // nb değişkeninin değerini result değişkeninin değerine eşitliyoruz.
+	if (power > 1) // power 
+		return (nb * ft_recursive_power(nb, (power - 1))); // ft_recursive_power(nb, (power - 1)) fonksiyonunun çağrısı ile çalışır ve her çağrı ile power değeri bir azaltılır. 
+	if (power == 0) // power değişkeninin değeri 0'a eşitse bu koşula giriyoruz.
+		return (1); // 1 döndürüp ekrana yazdırıyoruz.
+	if (power < 0) // power değişkeninin değeri 0'dan küçükse bu koşula giriyoruz.
+		return (0); // 0 döndürüp ekrana yazdırıyoruz.
+	return (result); // result değerini döndürüp ekrana yazdırıyoruz.
 }
 
-int	main(void)
+/*int	main(void)
 {
 	printf("%i", ft_recursive_power(5, 1));
-}
+}*/
