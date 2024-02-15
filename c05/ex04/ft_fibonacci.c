23cc1d962ff756839ee15a16df9bffc3cec11fbd
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-15 15:58:33 +0300

    ft_fibonacci.c Updated

--- c05/ex04/ft_fibonacci.c
+++ c05/ex04/ft_fibonacci.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:22:18 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:26:14 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/28 10:56:02 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/28 10:11:12 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_fibonacci(int index)
+int	ft_fibonacci(int index) // Fonksiyonumuzda 1 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	if (index < 0)
-		return (-1);
-	if (index < 2)
-		return (index);
-	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+	if (index < 0) // index değeri 0'dan küçükse bu koşula giriyoruz.
+		return (-1); // -1 döndürüp ekrana yazdırıyoruz.
+	if (index < 2) // index değeri 2'den küçükse bu koşula giriyoruz.
+		return (index); // index değerini döndürüp ekrana yazdırıyoruz.
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1)); // index değerinin 2 ve 1 öncesindeki index değerlerine ait fibonacci sayılarının toplamını döndürüyoruz.
 }
 
-int	main(void)
+/*int	main(void)
 {
 	printf("%i", ft_fibonacci(7));
-}
+}*/
