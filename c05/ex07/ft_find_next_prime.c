05648ecc1c54f90d300f2147ea00bd57b964961b
HasancanKahraman <hsncankhrmn@gmail.com>
2024-02-15 16:17:12 +0300

    ft_find_next_prime.c Updated

--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -1,46 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkahrama <hkahrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 11:22:18 by hkahrama          #+#    #+#             */
-/*   Updated: 2024/02/07 11:26:14 by hkahrama         ###   ########.fr       */
+/*   Created: 2024/01/28 11:44:11 by hkahrama          #+#    #+#             */
+/*   Updated: 2024/01/28 12:26:44 by hkahrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-int	ft_is_prime(int nb)
-{
-	int	i;
 
-	i = 2;
-	 if (nb < 2)
-	        return(0);
-	while (i <= nb / i)
+int	ft_is_prime(int nb) // Fonksiyonumuzda 1 adet integer tipinde parametre gönderilmesini istiyoruz.
+{
+	int	i; // i adında integer bir değişken tanımlıyoruz.
+
+	i = 2; // i değişkeninin değerini 2'ye eşitliyoruz
+	if (nb <= 1) // nb değişkeninin değeri 1'den küçükse/eşitse bu koşula giriyoruz.
+		return (0); // 0 döndürüp ekrana yazdırıyoruz.
+	while (i <= (nb / 2)) // i değişkeninin değeri nb değişkeninin değerinin 2'ye bölümünden küçükse/eşitse bu döngüye giriyoruz.
 	{
-		if (nb % i == 0)
-			return (0);
-		i++;
+		if (!(nb % i)) // nb değişkeninin değerinin i değişkeninin değerine modundan kalan varsa bu koşula giriyoruz.
+			return (0); // 0 döndürüp ekrana yazdırıyoruz.
+		else
+			i += 1; // i'nin değerini 1 arttırıyoruz.
 	}
-	return (1);
+	return (1); // 1 döndürüp ekrana yazdırıyoruz.
 }
 
-int	ft_find_next_prime(int nb)
+int	ft_find_next_prime(int nb) // Fonksiyonumuzda 1 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	if (nb < 2)
-		return (2);
-	while (nb >= 2)
+	if (nb < 2) // nb değişkeninin değeri 2'den küçükse bu koşula giriyoruz.
+		return (2); // 2 döndürüp ekrana yazdırıyoruz.
+	while (nb >= 2) // nb değişkeninin değeri 2'den büyükse/eşitse bu döngüye giriyoruz.
 	{
-		if (ft_is_prime(nb) == 1)
-			return (nb);
-		nb++;
+		if (ft_is_prime(nb) == 1) // ft_is_prime fonksiyonuna nb değerini gönderiyoruz, çıkan sonuç 1'e eşitse bu koşula giriyoruz.
+			return (nb); // nb değişkeninin değerini döndürüp ekrana yazdırıyoruz.
+		nb++; // nb'yi 1 arttırıyoruz.
 	}
-	return (0);
+	return (0); // 0 döndürüp ekrana yazdırıyoruz.
 }
 
-int main (void)
+/*int main (void)
 {
 	printf("%d -> %d\n", -2316, ft_find_next_prime(-2316));
 	printf("%d -> %d\n", 0, ft_find_next_prime(0));
@@ -53,4 +55,4 @@ int main (void)
 	printf("%d -> %d\n", 2147483646, ft_find_next_prime(2147483646));
 	printf("%d -> %d\n", 2147483647, ft_find_next_prime(2147483647));
 	return (0);
-}
+}*/
